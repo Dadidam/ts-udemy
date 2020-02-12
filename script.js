65df839b86fadb29f18b15c0cfb1b92230926b15
ivorontcov <iliav@hurdit.com>
2020-02-12 09:46:15 -0500

    Playing with type inference

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ var Color;
     Color[Color["Gray"] = 0] = "Gray";
     Color[Color["Green"] = 1] = "Green";
     Color[Color["Blue"] = 2] = "Blue";
-    Color[Color["Red"] = 777] = "Red"; // 3
+    Color[Color["Red"] = 777] = "Red";
 })(Color || (Color = {}));
 var myColor = Color.Red;
 console.log(myColor);
+// functions
+var realName = 'WIll';
+function getName() {
+    return realName;
+}
+// void
+function sayHello() {
+    console.log('Hi there!');
+}
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 3));
--- script.ts
+++ script.ts
@@ -10,4 +10,29 @@ enum Color {
 }
 
 let myColor: Color = Color.Red;
-console.log(myColor);
+// console.log(myColor);
+
+
+// functions
+let realName = 'WIll';
+function getName(): string {
+  return realName;
+}
+// console.log(getName());
+
+// void
+function sayHello(): void {
+  console.log('Hi there!');
+}
+// console.log(sayHello());
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+// console.log(multiply(2, 3));
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+myMultiply = multiply;
+console.log(myMultiply(3, 4));
